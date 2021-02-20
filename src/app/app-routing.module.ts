e368568ca84e42343320b6eab290860fa5abc661
Rifkhan Mohamed <rifkhanabdullah@gmail.com>
2021-02-20 12:12:06 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "products", loadChildren : () => import('./products/products.module').then(m=>m.ProductsModule)},
+  {path : "branches", loadChildren : () => import('./branches/branches.module').then(m=>m.BranchesModule)},
+  {path : "suppliers", loadChildren : () => import('./suppliers/suppliers.module').then(m=>m.SuppliersModule)},
+  {path : "login", loadChildren : () => import('./login/login.module').then(m=>m.LoginModule)},
+  {path : "categories", loadChildren : () => import('./categories/categories.module').then(m=>m.CategoriesModule)}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavBarServicesService } from "src/app/_services/nav-bar-services.service";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pos';
+  visible=false;
+  constructor(private router: Router,public nav :NavBarServicesService) {
+
+  }
+  ngOnInit(): void {
+    this.nav.show();
+  }
+  openNav(){
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("main1").style.marginLeft="250px";
+  }
+  closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("main1").style.marginLeft="0";
+  }
+  navigateProducts(){
+    this.router.navigate(['products/get']);
+  }
+  navigateBranches(){
+    this.router.navigate(['branches/get']);
+  }
+  navigateSuppliers(){
+    this.router.navigate(['suppliers/get']);
+  }
 }
