b5516146e3c45590799184a2231b5eb96b9ba823
Rifkhan Mohamed <rifkhanabdullah@gmail.com>
2021-02-22 23:14:45 +0530

    signup function created

--- src/app/signup/Components/sign-up/sign-up.component.ts
+++ src/app/signup/Components/sign-up/sign-up.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { RxwebValidators } from '@rxweb/reactive-form-validators';
+import { ToastrService } from 'ngx-toastr';
 import { NavBarServicesService } from "src/app/_services/nav-bar-services.service";
 import { SignUpService } from "../../Services/sign-up.service";
 @Component({
@@ -9,43 +11,109 @@ import { SignUpService } from "../../Services/sign-up.service";
 })
 export class SignUpComponent implements OnInit {
   Towns=[];
-  form=new FormGroup({
-    name:new FormControl('',[Validators.required]),
-    nic:new FormControl('',[Validators.required]),
-    address:new FormControl('',[Validators.required]),
-    email:new FormControl('',[Validators.required,Validators.email,this.isMailExist.bind(this)]),
-    town:new FormControl('',[Validators.required]),
-    mobile:new FormControl('',[Validators.required]),
-    land:new FormControl(''),
-    fax:new FormControl('')
-  })
+  mailDetails=[];
+  phoneDetails=[];
+  nicDetails=[];
+  form=new FormGroup({});
+  forMail=false;
 
-  constructor(private nav: NavBarServicesService,private appService: SignUpService) { }
+  constructor(private toastr: ToastrService,private nav: NavBarServicesService,private fb: FormBuilder,private appService: SignUpService) { 
+    this.nav.hide(); 
+    this.form=fb.group({
+      name:new FormControl('',[Validators.required]),
+      nic:new FormControl('',[Validators.required,this.isNicExist.bind(this)]),
+      address:new FormControl('',[Validators.required]),
+      email:new FormControl('',[Validators.required,Validators.email,this.isMailExist.bind(this)]),
+      town:new FormControl('',[Validators.required]),
+      mobile:new FormControl('',[Validators.required,Validators.pattern("[0-9 ]{10}"),this.isPhoneExist.bind(this)]),
+      land:new FormControl('',[Validators.pattern("[0-9 ]{10}")]),
+      fax:new FormControl('',[Validators.pattern("[0-9 ]{10}")])
+    })
+  }
 
   ngOnInit(): void {
-    this.nav.hide(); 
     this.appService.getAllTowns().toPromise()
     .then(res=>{
       this.Towns=res;
-      console.log(this.Towns);
       this.Towns.sort((a,b) => (a.cityName > b.cityName) ? 1 : ((b.cityName > a.cityName) ? -1 : 0)); 
     })
     .catch(e=>{
       console.log(e);
     });
-  }
 
-  isMailExist(control: FormControl){
-    this.appService.isExistMail(control.value).toPromise()
+    this.appService.userMailGetAll().toPromise()
     .then(res=>{
-      if(res==true){
-        return {'userEmailIsNotAllowed':true}
-      }
-      return false;
+      this.mailDetails=res;
+    })
+    .catch(e=>{
+      console.log(e);
+    });
+
+    this.appService.userPhoneGetAll().toPromise()
+    .then(res=>{
+      this.phoneDetails=res;
+    })
+    .catch(e=>{
+      console.log(e);
+      
+    });
+
+    this.appService.userNicGetAll().toPromise()
+    .then(res=>{
+      this.nicDetails=res;
     })
     .catch(e=>{
       console.log(e);
     })
   }
+
+  isMailExist(control: FormControl){
+    if(this.mailDetails.indexOf(control.value)!=-1){
+      return {'userEmailIsNotAllowed':true}
+    }
+    return null;
+  }
+
+  isPhoneExist(control: FormControl){
+    if(this.phoneDetails.indexOf(control.value)!=-1){
+      return {'userPhoneIsNotAllowed':true}
+    }
+    return null;
+  }
+
+  isNicExist(control: FormControl){
+    if(this.nicDetails.indexOf(control.value)!=-1){
+      return {'userNicIsNotAllowed':true}
+    }
+    return null;
+  }
+
+  onRegister(){
+    let body={
+      "userDetails": {
+        "correspondanceaddress": this.form.get('address').value,
+        "email": this.form.get('mail').value,
+        "emailverifiedid": true,
+        "faxno": this.form.get('fax').value,
+        "lanNo": this.form.get('land').value,
+        "mobileno": this.form.get('mobile').value,
+        "mobileverifieid": true,
+        "namewithinitials": this.form.get('name').value,
+        "nic": this.form.get('nic').value,
+        "town": this.form.get('town').value
+      }
+    }
+    console.log(body,"body");
+    
+    this.appService.signUp(body).toPromise()
+    .then(res=>{
+      this.toastr.success(res);
+    })
+    .catch(e=>{
+      this.toastr.error(e);
+      console.log(e);
+    })
+  }
+
 
 }
--- src/app/signup/Services/sign-up.service.ts
+++ src/app/signup/Services/sign-up.service.ts
@@ -10,6 +10,9 @@ export class SignUpService {
   
   private GetAllTowns= this.url+"/city/get/all";
   private IsExistMail=this.url+"/isExist";
+  private UserMailGetAll=this.url+"/userMail/get/all";
+  private UserPhoneGetAll=this.url+"/userPhone/get/all";
+  private UserNicGetAll=this.url+"/userNic/get/all";
   private SignUp=this.url+"/api/auth/signup";
 
   constructor(private http: HttpClient) { }
@@ -20,5 +23,21 @@ export class SignUpService {
   isExistMail(mail): Observable<any>{
     const url=`${this.IsExistMail}/${mail}`
     return this.http.get<any>(url);
+  }
+  userMailGetAll(): Observable<any>{
+    const url=`${this.UserMailGetAll}`
+    return this.http.get<any>(url);
+  }
+  userPhoneGetAll(): Observable<any>{
+    const url=`${this.UserPhoneGetAll}`
+    return this.http.get<any>(url);
+  }
+  userNicGetAll(): Observable<any>{
+    const url=`${this.UserNicGetAll}`
+    return this.http.get<any>(url);
+  }
+  signUp(body): Observable<any>{
+    const url=`${this.SignUp}`
+    return this.http.post<any>(url,body);
   }
 }
