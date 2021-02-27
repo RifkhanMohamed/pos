5cb243a5273d73d7bc1dbb379cc191dfe393c4a9
Rifkhan Mohamed <rifkhanabdullah@gmail.com>
2021-02-28 01:05:12 +0530

    commit

--- src/app/home/Components/home/home.component.ts
+++ src/app/home/Components/home/home.component.ts
@@ -1,18 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { NavBarServicesService } from "src/app/_services/nav-bar-services.service";
+import { HostListener } from "@angular/core";
+import { HomeService } from "../../Services/home.service";
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  Categories=[];
+  scrHeight:any;
+  scrWidth:any;
+  @HostListener('window:resize', ['$event'])
+   getScreenSize(event?) {
+        this.scrHeight = (window.innerHeight-250).toString()+'px';
+        this.scrWidth = window.innerWidth;
+        console.log(this.scrHeight, this.scrWidth);
 
-  constructor(private nav: NavBarServicesService) {
+  }
+
+
+
+  constructor(private nav: NavBarServicesService,private home:HomeService) {
     this.nav.show();
+    this.getScreenSize();
+    
    }
 
   ngOnInit(): void {
-   
+    this.home.getAllCategories().toPromise()
+    .then(res=>{
+      this.Categories=res;
+      console.log(this.Categories);
+      
+    })
+    .catch(e=>{
+      console.log(e);
+      
+    });
   }
 
 }
+
+
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { authInterceptorProviders } from "src/app/_helpers/auth.interceptor";
 import { HomeComponent } from "./Components/home/home.component";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { AgGridModule } from 'ag-grid-angular';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { RxReactiveFormsModule } from '@rxweb/reactive-form-validators';
+import { TagInputModule } from 'ngx-chips';
 
 
 const routes: Routes = [
@@ -18,7 +29,22 @@ const routes: Routes = [
   declarations: [],
   imports: [
     CommonModule,
-    [RouterModule.forChild(routes)]
+    [RouterModule.forChild(routes)],
+    FormsModule,
+    NgxPaginationModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatSortModule,
+    AgGridModule.withComponents([]),
+    Ng2SearchPipeModule,
+    FormsModule,
+    NgxPaginationModule,
+    ReactiveFormsModule,
+    RxReactiveFormsModule,
+    TagInputModule
   ],
   providers:[
     authInterceptorProviders
